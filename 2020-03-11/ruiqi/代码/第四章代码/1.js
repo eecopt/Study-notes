3ba2037485ea62328adda5531c2c268ae5cb748f
Aes-Rseb <2114200289@qq.com>
2020-03-12 22:17:55 +0800

    2020-03-12
    代码笔记

---
+++ 2020-03-11/ruiqi/代码/第四章代码/1.js
@@ -0,0 +1,33 @@
+let message;
+
+message = 'Hello'; // 保存字符串
+alert(message); // 显示变量内容
+
+let messags = 'Hello!'; // 定义变量，并且赋值
+//另一种简便写法
+alert(messags); // Hello!
+
+let user = 'John', age = 25, messago = 'Hello';//声明多个变量
+
+let useo = 'John';
+let agi = 25;
+let messaga = 'Hello';//声明多个变量写法规范
+
+let usec = 'John',
+    agb = 25,
+    messagl = 'Hello';//多样的写法一
+
+let useu = 'John'
+    , agv = 25
+    , messagz = 'Hello';//逗号在前的写法
+
+let hello = 'Hello world!';
+
+let messagp;
+
+// 将字符串 'Hello world' 从变量 hello 拷贝到 messagp
+messagp = hello;
+
+// 现在两个变量保存着相同的数据
+alert(hello); // Hello world!
+alert(messagp); // Hello world!
