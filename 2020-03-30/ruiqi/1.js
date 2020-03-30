5160001bf22fb43cdbee277ef7d04af8778cb714
Aes-Rseb <2114200289@qq.com>
2020-03-30 21:20:38 +0800

    2020-03-30
    ruiqi

---
+++ 2020-03-30/ruiqi/1.js
@@ -0,0 +1,11 @@
+const myBirthday = '18.04.1982';  //const设置一个常量
+
+//大写形式的函数
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ……当我们需要选择一个颜色
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
